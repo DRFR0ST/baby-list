7249f3fb2d486a4fc4df2cd694d8c4d5942e3ad0
Mike Eling <mike.eling97@gmail.com>
2020-07-10 10:57:35 +0200

    :wrench: Replace BrowserRouter with HashRouter

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "react-jss";
 import { Theme } from '../types';
 import AppBar from './common/AppBar';
