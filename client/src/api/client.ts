03c7dcc81652bf6a449295eaf9249823c5f698d6
Mike Eling <mike.eling97@gmail.com>
2020-07-21 15:36:41 +0200

    :wrench: Fix CORS

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -33,18 +33,12 @@ export class Client implements IClient {
                 
             }
 
-            fetch(reqUrl, {
-                method: 'POST',
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'omit', // include, *same-origin, omit
-                headers: {
-                  'Content-Type': 'application/json',
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            })
+            let headers = new Headers({
+                'Access-Control-Allow-Origin':'*',
+                'Content-Type': 'multipart/form-data'
+            });
+
+            fetch(reqUrl, { mode: "cors", headers })
                 .then(response => response.json())
                 .then(handleResolve)
                 .catch(reject);
